730a0b98a6b40c456ca5b01486b599176a9cf0b1
sarcasticoder <sarcasticoder7@gmail.com>
2022-07-20 17:44:47 +0530

    index

--- third.ts
+++ third.ts
@@ -1,13 +1,10 @@
 console.log("Abhishek's Changes");
 
 // branch s7 commit
-<<<<<<< HEAD
 // cherry-pick commit 2
 // branch s7 commit
 
 
 // commit for cherry pick
-=======
 
 // Cherry-pick 1 commit
->>>>>>> s7
